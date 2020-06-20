9df744eb19f67fc422cf73d7b1e1c45f96f1645c
hugueds <hugo-pdf@hotmail.com>
2020-06-20 14:13:39 -0300

    add tflite project

---
+++ main.py
@@ -0,0 +1,85 @@
+import tensorflow as tf
+import numpy as np
+import cv2 as cv
+
+cap = cv.VideoCapture(0)
+
+interpreter = tf.lite.Interpreter('detect.tflite')
+
+interpreter.allocate_tensors()
+
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+height = input_details[0]['shape'][1]
+width = input_details[0]['shape'][2]
+
+floating_model = (input_details[0]['dtype'] == np.float32)
+
+input_mean = 127.5
+input_std = 127.5
+
+output_data = interpreter.get_tensor(output_details[0]['index'])
+
+def load_labels(path):
+    with open(path, 'r') as f:
+        return [line.strip() for line in f.readlines()]
+
+labels = load_labels('labelmap.txt')
+
+if labels[0] == '???':
+    del(labels[0])
+
+while True:
+
+    _, frame = cap.read()    
+    frame = frame.copy()
+
+    key = cv.waitKey(2) & 0xFF
+    
+    frame_rgb = frame.copy()
+    frame_resized = cv.resize(frame_rgb, (300, 300))    
+
+    input_data = np.expand_dims(frame_resized, axis=0)
+    input_data = (2.0 / 255.0) * input_data - 1.0
+    input_data = input_data.astype('float32')
+
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+    interpreter.invoke()
+
+    boxes = interpreter.get_tensor(output_details[0]['index'])[0] # Bounding box coordinates of detected objects
+    classes = interpreter.get_tensor(output_details[1]['index'])[0] # Class index of detected objects
+    scores = interpreter.get_tensor(output_details[2]['index'])[0] # Confidence of detected objects
+    num = interpreter.get_tensor(output_details[3]['index'])[0]  # Total number of detected objects (inaccurate and not needed)
+    
+
+    for i in range(len(scores)):
+        if ((scores[i] > 0.5) and (scores[i] <= 1.0)):
+
+            imW, imH = 640,480
+            # Get bounding box coordinates and draw box
+            # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
+            ymin = int(max(1,(boxes[i][0] * imH)))
+            xmin = int(max(1,(boxes[i][1] * imW)))
+            ymax = int(min(imH,(boxes[i][2] * imH)))
+            xmax = int(min(imW,(boxes[i][3] * imW)))
+            
+            cv.rectangle(frame, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
+
+            # Draw label
+            object_name = labels[int(classes[i])] # Look up object name from "labels" array using class index
+            label = '%s: %d%%' % (object_name, int(scores[i]*100)) # Example: 'person: 72%'
+            labelSize, baseLine = cv.getTextSize(label, cv.FONT_HERSHEY_SIMPLEX, 0.7, 2) # Get font size
+            label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
+            cv.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv.FILLED) # Draw white box to put label text in
+            cv.putText(frame, label, (xmin, label_ymin-7), cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
+
+       
+
+    cv.imshow('Main', frame)
+    
+    if key == ord('q'):
+        break
+
+
+cap.release()
+cv.destroyAllWindows()
